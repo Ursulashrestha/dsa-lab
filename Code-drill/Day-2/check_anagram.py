c27a1cc8d3bda790b5a0e94013908b40e37d459d
Ursulashrestha <scrappermoon1999@gmail.com>
2025-06-27 21:55:49 +0200

    Day2

--- Code-drill/Day-2/check_anagram.py
+++ Code-drill/Day-2/check_anagram.py
@@ -5,8 +5,8 @@ from collections import Counter
 def isAnagram(s: str, t: str) -> bool:
     return Counter(s) == Counter(t)
 
-print(isAnagram("listen", "silent"))  # True
-print(isAnagram("hello", "hell"))     # False
+# print(isAnagram("listen", "silent"))  # True
+# print(isAnagram("hello", "hell"))     # False
 print(isAnagram("anagram", "nagaram"))  # True
 print(isAnagram("rat", "car"))          # False
 
