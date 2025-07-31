e98f43ba62332e4126829a9f2b411bbd8c4f14ad
Ursulashrestha <scrappermoon1999@gmail.com>
2025-07-31 16:18:05 +0200

    array and hashing easy problem solved

--- arrays_hashing/easy/concatenation_of_array.py
+++ arrays_hashing/easy/concatenation_of_array.py
@@ -12,6 +12,20 @@ def concatArray(nums: list[int])-> list[int]:
 # Time complexity: O(n)
 # Space complexity: O(n)
 
+def concatenateArray(nums: list[int]) -> list[int]:
+    # Get the length of the list
+    n = len(nums)
+    # Creating a list to store the result which is twice the size of nums
+    ans=[0] * (2*n) # [0,0,0,0,0,0]
+    # ans[i] == nums[i]= ans[i+n]
+    for index,num in enumerate(nums):
+        ans[index] = num = ans[index+num]
+        # loop one : i=0, num=2, n=3
+        # ans[0] = ans[0+2]=> ans[2] = 2
+        # ans[0]=2
+        # ans[2] = 2
+        #[2,0,0,2,0,0]-- after the first loop
+    return ans
 
 # Alternative
 # def getConcatenate(nums: list[int]) -> list[int]:
