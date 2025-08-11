7e4fd0ca7f8be6a4d88634f1d4750d2b8a297376
Ursulashrestha <scrappermoon1999@gmail.com>
2025-08-11 11:47:04 +0200

    array and hashing medium problem solved

--- arrays_hashing/medium/encode_decode.py
+++ arrays_hashing/medium/encode_decode.py
@@ -1,11 +1,25 @@
 # Encode and Decode String
-strs = ["I","am", "tired"]
-output = "1#I2#am5#tired" # for encode
-output2 = ["I","am", "tired"] # for decode
+strs = ["hello","world"]
+output = "5#hello#world" # for encode
+output2 = ["hello","world"] # for decode
 
 def encode(strs:list[str])-> str:
     res=""
     for s in strs:
         res += str(len(s))+ "#"+ s
+    return res
+
+def decode(s:str)-> list[str]:
+    res = []
+    i = 0
+    while i < len(s):
+        j = i # j as new pointer to find #
+        while s[j] != "#":
+            j += 1 # move j until found #
+        length = s[i:j]  # s[0:1] -> '5', s[0:2]-> '5#'
+        i = j + 1
+        j = i + length
+        res.append(s[i:j])
+        i = j
     return res
 
