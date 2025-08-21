d5cc2d555894c689b4c93e7f0926066de4ab3cda
Ursulashrestha <scrappermoon1999@gmail.com>
2025-08-21 13:10:46 +0200

    two pointers easy problem solved

--- two_pointers/easy/valid_palindrome.py
+++ two_pointers/easy/valid_palindrome.py
@@ -10,5 +10,25 @@ def isPalindrome(s:str)-> bool:
             newStr += char.lower()
     return newStr == newStr[::-1]
 
-#Time Complexity = O(n)
-#Space Complexity = O(n)
+# Time Complexity = O(n)
+# Space Complexity = O(n)
+
+# Alternative approach (Two Pointers)
+def isPalindromeI(self,s:str)-> bool:
+    l = 0
+    r = len(s)-1
+    while l<r:
+        while l<r and not self.alphaNum(s[l]):
+            l +=1
+        while r > l and not self.alphaNum(s[r]):
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l = l+ 1
+        r = r - 1
+    return True
+
+def alphaNum(self,c):
+    return (ord('a')<= ord(c) <= ord('z')or
+            ord('A')<= ord(c) <= ord('Z') or
+            ord('0')<= ord(c) <= ord('9'))
