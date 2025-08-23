e94f322c78130413dac48390af1d0e7d5a3f1292
Ursulashrestha <scrappermoon1999@gmail.com>
2025-08-23 19:02:40 +0200

    two pointers medium problem solved

---
+++ two_pointers/medium/container_with_most_water.py
@@ -0,0 +1,17 @@
+height = [1,8,6,2,5,4,8,3,7]
+#output = int -- an area
+
+def maxArea(height:list[int])-> int:
+    l = 0
+    r = len(height)-1
+    res = 0
+    while l<r:
+        w = r-l
+        h = min(height[l], height[r])
+        area = w*h
+        res = max(area, res)
+        if height[l]<= height[r]:
+            l+=1
+        else:
+            r-=1
+    return res
