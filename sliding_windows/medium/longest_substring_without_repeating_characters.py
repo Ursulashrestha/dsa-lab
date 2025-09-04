46fac099e5e350de79e5ead13a2d6fc003c2ee10
Ursulashrestha <scrappermoon1999@gmail.com>
2025-09-04 10:56:51 +0200

    sliding window medium question solved

---
+++ sliding_windows/medium/longest_substring_without_repeating_characters.py
@@ -0,0 +1,13 @@
+#Longest Substring without repeating character
+strs = 'abcabcbb'
+#output = 3
+def longestlengthSubstring(strs:str)-> int:
+    mp = {} #stores the last place the letter was found
+    l = 0
+    res = 0
+    for r in range(len(strs)):
+        if strs[r] in mp: #if number is found in mp
+            l = max(mp[strs[r]]+1,l) #move l skipping the previous/old char and make sure it doesnt move backward
+        mp[strs[r]]= r #update the index in mp for repeated char
+        res = max(res, r-l+1)
+    return res
